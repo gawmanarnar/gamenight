d1356dbd55c86b4243509e885c894c1f2f9d2b34
Tyler <tylerjgaw@gmail.com>
2016-01-22 21:41:37 -0500

    added shuffle function flushing out gamenight creation in the db.

--- server.js
+++ server.js
@@ -12,19 +12,36 @@ var app = express();
 
 // Load models
 var Gamenight = require('./models/gamenight');
+var Game = require('./models/game');
+var Player = require('./models/player');
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 app.get('/', function (req, res) {
-    //console.log('Last Wednesday: ' + moment().day(-4).format('ddd, MMM DD')); // Wednesday - 7 = (3) - 7 = - 4
-    //console.log('This Wednesday: ' + moment().day(3).format('ddd, MMM DD')); // Wednesday is 3
-    //console.log('Next Wednesday: ' + moment().day(10).format('ddd, MMM DD')); // Wednesday + 7 = 3 + 7 = 10
 
-    var thisWednesday = moment().day(3).startOf('day');
-    var thisWednesdayEnd = moment(thisWednesday).endOf('day');
+    // In moment, this week's Wednesday corresponds to 3. Next week's Wednesday corresponds to 10 (3 + 7 = 10).
+    // If Wednesday has passed in the current week (day > 3), we should be looking to next Wednesday.
+    var wednesday =  moment().day(moment().day() > 3 ? 10 : 3).startOf('day');
+    var wednesdayEnd = moment(wednesday).endOf('day');
 
-    console.log(thisWednesday.toDate());
-    console.log(thisWednesdayEnd.toDate());
-
-    Gamenight.findOne({date: { $gte: thisWednesday.toDate(), $lt: thisWednesdayEnd.toDate() }}, function(err, night) {
+    Gamenight.findOne({date: { $gte: wednesday.toDate(), $lt: wednesdayEnd.toDate() }}, function(err, night) {
         if(err) {
             console.log(err);
         } else {
@@ -34,7 +51,33 @@ app.get('/', function (req, res) {
             }
 
             var newGamenight = new Gamenight();
-            newGamenight.date = thisWednesday;
+            newGamenight.date = wednesday;
+
+            Player.find({}, function(err, players) {
+                shuffle(players);
+
+                players.forEach(function(player) {
+                    console.log(player.name);
+                });
+
+                while(players.length > 0 ) {
+                    console.log('here');
+                    var newGame = Game();
+
+                    var player1 = players.pop();
+                    newGame.player1 = player1.name;
+
+                    if(players.length > 0) {
+                        var player2 = players.pop();
+                        newGame.player2 = player2.name;
+                        player2.played.push(newGame.player1);
+                        player1.played.push(newGame.player2);
+                    } else {
+                        player1.played.push("");
+                    }
+                    newGamenight.games.push(newGame);
+                }
+            });
 
             newGamenight.save(function (error) {
                 if(error) {
