b18a0f8c50b884dfd5017dd21378874182760824
Tyler <tylerjgaw@gmail.com>
2016-01-28 23:42:40 -0500

    added logging for getmercd endpoint

--- server.js
+++ server.js
@@ -108,6 +108,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/getmercd', function (req, res) {
+
+    console.log('http://' + req.headers.host + '/getmercd.gif');
+
     res.status(200).send({
         "response_type": 'in_channel',
         "attachments": [
